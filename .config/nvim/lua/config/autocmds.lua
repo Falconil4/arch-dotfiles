3f02ab05813dc5027eb10ddeef600758c5a54f5a
falco <kstaniek95@gmail.com>
2025-05-31 10:48:05 +0200

    adjustments after work pc config

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,20 +1,28 @@
 vim.api.nvim_create_autocmd("BufWritePre",
-	{
-		pattern = "*",
-		command = "Neoformat"
-	}
+  {
+    pattern = "*",
+    callback = function() vim.lsp.buf.format() end
+  }
 )
 
 vim.api.nvim_create_autocmd({ "LspAttach", "InsertLeave" },
-	{
-		pattern = "*",
-		callback = function() vim.lsp.codelens.refresh() end,
-	}
+  {
+    pattern = "*",
+    callback = function() vim.lsp.codelens.refresh() end,
+  }
 )
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-	pattern = "*",
-	callback = function()
-		vim.highlight.on_yank { higroup = "Visual", timeout = 300 }
-	end,
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank { higroup = "Visual", timeout = 300 }
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "lua",
+  callback = function()
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
+  end,
 })
--- .config/nvim/lua/config/opts.lua
+++ .config/nvim/lua/config/opts.lua
@@ -1,24 +1,28 @@
 if (os.getenv('WSL_INTEROP') ~= nil) then
-	vim.g.clipboard = {
-		name = 'WslClipboard',
-		copy = {
-			['+'] = 'clip.exe',
-			['*'] = 'clip.exe',
-		},
-		paste = {
-      ['+'] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-      ['*'] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-		},
-		cache_enabled = 0,
-	}
+  vim.g.clipboard = {
+    name = 'WslClipboard',
+    copy = {
+      ['+'] = 'clip.exe',
+      ['*'] = 'clip.exe',
+    },
+    paste = {
+      ['+'] =
+      'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ['*'] =
+      'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = 0,
+  }
 end
 
 vim.opt.clipboard = 'unnamedplus'
 vim.opt.number = true
 vim.opt.wrap = false
 vim.opt.relativenumber = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.smarttab = true
+vim.opt.expandtab = true
 vim.opt.spell = false
 vim.opt.signcolumn = 'yes'
 vim.g.snacks_animate = false
